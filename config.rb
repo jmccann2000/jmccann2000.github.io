066e39c3e5dd3ef06a8a77a6d7980593be7c354a
Jake McCann <jake.mccann@cadtalk.com>
2022-08-06 18:46:40 -0400

    update index

--- config.rb
+++ config.rb
@@ -13,6 +13,8 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+set :build_dir, '../jmccann2000.github.io'
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
